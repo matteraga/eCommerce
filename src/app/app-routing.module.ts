d9100393d026d326b6b9b053eafa1c7878139eb7
Hider <alessio5.magnani@gmail.com>
2023-06-07 12:57:12 +0200

    Add search route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from "./app.component";
 import { CartComponent } from './components/cart/cart.component';
-import { HomeComponent } from './components/home/home.component';
+import {ProductPaginationComponent} from "./components/product-pagination/product-pagination.component";
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path : 'home', component : HomeComponent},
-  { path: 'cart', component: CartComponent }
+  { path: '', component: AppComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'search/:search/:page', component: ProductPaginationComponent },
 ];
 
 @NgModule({
