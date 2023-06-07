c7b4b4c0a1192c059bea48c2e2df0f1e0b48c0cf
Mattia Golinucci <golinucci.mattia@gmail.com>
2023-06-07 15:37:18 +0200

    fix routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from "./app.component";
 import { CartComponent } from './components/cart/cart.component';
 import {ProductPaginationComponent} from "./components/product-pagination/product-pagination.component";
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  { path: '', component: AppComponent },
+  { path: '', component: HomeComponent },
   { path: 'cart', component: CartComponent },
   { path: 'search/:search/:page', component: ProductPaginationComponent },
 ];
