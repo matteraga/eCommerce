3709f32fc187db6355f37413594240e7a80fba6e
Hider <alessio5.magnani@gmail.com>
2023-06-07 16:09:00 +0200

    Edit product pagination component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from "./app.component";
 import { CartComponent } from './components/cart/cart.component';
 import {ProductPaginationComponent} from "./components/product-pagination/product-pagination.component";
 import { HomeComponent } from './components/home/home.component';
