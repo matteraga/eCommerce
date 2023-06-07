152d3070eceb922ffc34372bb2fc3885f840b7d4
Hider <alessio5.magnani@gmail.com>
2023-06-07 11:29:37 +0200

    Create product service

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,41 @@
+import {Injectable} from "@angular/core";
+import {Product} from "../models/product";
+import {Category} from "../models/category";
+
+@Injectable()
+export class ProductService {
+  products: Product[] = [];
+
+  getById(id: number): Product | undefined {
+    return this.products.find(p => p.id == id);
+  }
+
+  getCategories(): Category[] {
+    return Object.values(Category);
+  }
+
+  getProductsByCategory(category: Category, page: number = 1, pageSize: number = 20): Product[] {
+    return this.getProducts(p => p.category == category, page, pageSize);
+  }
+
+  getProductsByName(search: string, page: number = 1, pageSize: number = 20): Product[] {
+    return this.getProducts(p => p.name.toLowerCase().includes(search.toLowerCase()), page, pageSize);
+  }
+
+  private getProducts(predicate: (p: Product) => boolean, page: number = 1, pageSize: number = 20): Product[] {
+    return this.products
+      .filter(predicate)
+      .slice((page - 1) * pageSize, page * pageSize);
+  }
+
+  getRandomCategory(): Category {
+    const randomIndex = Math.floor(Math.random() * this.getCategories().length);
+    return this.getCategories()[randomIndex];
+  }
+
+  getRandomProduct(category: Category = this.getRandomCategory()): Product {
+    const products = this.getProductsByCategory(category);  // Get random only from first page
+    const randomIndex = Math.floor(Math.random() * products.length);
+    return products[randomIndex];
+  }
+}
