41580e9a7a84d23c4d60fdd044ed84dae109c89c
Mattia Golinucci <golinucci.mattia@gmail.com>
2023-06-07 16:57:24 +0200

    add searchCategory

---
+++ src/app/components/category/category.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Product } from 'src/app/models/product';
+
+@Component({
+  selector: 'app-category',
+  templateUrl: './category.component.html',
+  styleUrls: ['./category.component.css']
+})
+export class CategoryComponent 
+{
+  /*productCategory : Product[];
+  constructor(private productService : ProductService){
+    this.productCategory = this.productService.
+  }*/
+}
