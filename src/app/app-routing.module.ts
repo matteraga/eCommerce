9aa585fd1572f040428dd84474397bf21f8c5175
Hider <alessio5.magnani@gmail.com>
2023-06-07 11:34:58 +0200

    Fix routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
+import {AppComponent} from "./app.component";
 
 const routes: Routes = [
-  { path : '', component : HeaderComponent},
+  { path : '', component : AppComponent }
 ];
 
 @NgModule({
