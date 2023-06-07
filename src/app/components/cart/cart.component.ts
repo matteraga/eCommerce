c6f89838f880c385b12029728a54edd699722dbe
matteraga <matteoraganini@gmail.com>
2023-06-07 20:59:32 +0200

    Edit cart & header componets

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent {
+  constructor(private cartService: CartService) { }
 
+  totale = this.cartService.total()
 }
