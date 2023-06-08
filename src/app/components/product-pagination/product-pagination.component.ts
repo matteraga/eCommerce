a873e84ca8c7fe5165b8cf4ede327459a5c02c23
Mattia Golinucci <golinucci.mattia@gmail.com>
2023-06-08 11:20:54 +0200

    edit productPagination

--- src/app/components/product-pagination/product-pagination.component.ts
+++ src/app/components/product-pagination/product-pagination.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {Product} from "../../models/product";
 import {ProductService} from "../../services/product.service";
 import {ActivatedRoute} from "@angular/router";
@@ -9,40 +9,6 @@ import {ActivatedRoute} from "@angular/router";
   styleUrls: ['./product-pagination.component.css']
 })
 export class ProductPaginationComponent {
-  products: Product[];
-  page: number;
-  searchString: string;
-  productPageCount: number;
-
-  constructor(private productService: ProductService, private routeService: ActivatedRoute) {
-    this.searchString = this.routeService.snapshot.params['search'];
-    this.page = +this.routeService.snapshot.params['page'];
-
-    this.products = this.productService.getProductsByName(this.searchString, this.page);
-    this.productPageCount = this.productService.productPageCount(this.searchString);
-  }
-
-  goToPage(page: number) {
-    if (page < 1 || page > this.productPageCount)
-      return;
-
-    this.page = page;
-    this.products = this.productService.getProductsByName(this.searchString, this.page);
-  }
-
-  prevPage() {
-    if (this.page <= 1)
-      return;
-
-    this.page--;
-    this.goToPage(this.page);
-  }
-
-  nextPage() {
-    if (this.page >= this.productPageCount)
-      return;
-
-    this.page++;
-    this.goToPage(this.page);
-  }
+  @Input()
+  products?: Product[];
 }
