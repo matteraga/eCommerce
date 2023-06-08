41a219269b6c8856b37c407e98480d79fbd5a258
Hider <alessio5.magnani@gmail.com>
2023-06-08 10:41:19 +0200

    Add search result component

---
+++ src/app/components/search-result/search-result.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import {Product} from "../../models/product";
+import {ProductService} from "../../services/product.service";
+import {ActivatedRoute} from "@angular/router";
+
+@Component({
+  selector: 'app-search-result',
+  templateUrl: './search-result.component.html',
+  styleUrls: ['./search-result.component.css']
+})
+export class SearchResultComponent {
+  products: Product[];
+  searchString: string;
+
+  constructor(private productService: ProductService, private route: ActivatedRoute) {
+    this.searchString = this.route.snapshot.params['searchString'];
+    this.products = this.productService.getProductsByName(this.searchString);
+  }
+}
