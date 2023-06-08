5d939939fc87e72ae583687fe2921e1e6aa785b4
Hider <alessio5.magnani@gmail.com>
2023-06-08 10:41:02 +0200

    Edit slide products

--- src/app/components/slide-products/slide-products.component.ts
+++ src/app/components/slide-products/slide-products.component.ts
@@ -1,15 +1,17 @@
 import { Component, Input } from '@angular/core';
 import { Category } from 'src/app/models/category';
 import { Product } from 'src/app/models/product';
-import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-slide-products',
   templateUrl: './slide-products.component.html',
   styleUrls: ['./slide-products.component.css']
 })
-export class SlideProductsComponent 
+export class SlideProductsComponent
 {
   @Input()
-  products : Product[] = [];
+  products? : Product[];
+
+  @Input()
+  category? : Category;
 }
