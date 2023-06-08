dcdb845bd0ecbedfea503b3a6f9c534dfe5be77d
Hider <alessio5.magnani@gmail.com>
2023-06-08 23:16:37 +0200

    Add page navigator in category search result

--- src/app/components/category-result/category-result.component.ts
+++ src/app/components/category-result/category-result.component.ts
@@ -13,13 +13,18 @@ export class CategoryResultComponent {
   products: Product[];
   category: Category;
   page: number;
-  productsPageCount: number;
+  maxPage: number;
 
   constructor(private productService: ProductService, private routeService: ActivatedRoute) {
     this.category = this.routeService.snapshot.params['category'] as Category
     this.page = 1;
 
     this.products = this.productService.getProductsByCategory(this.category, this.page);
-    this.productsPageCount = this.productService.categoryPageCount(this.category);
+    this.maxPage = this.productService.categoryPageCount(this.category);
+  }
+
+  changePage(page: number) {
+    this.page = page;
+    this.products = this.productService.getProductsByCategory(this.category, this.page);
   }
 }
